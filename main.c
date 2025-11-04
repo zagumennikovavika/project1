c306acb6b3bf8453d46ad2e78e87256014a9e754
zagumennikovavika <zagumennikovavs@gmail.com>
2025-11-04 14:08:16 +0700

    changed

--- main.c
+++ main.c
@@ -2,16 +2,16 @@
 #include "student1.h"
 
 int main(void) {
-    int src_base = 10;
-    int dest_base = 2;
-    const char* number = "13";
-    
+    int src_base = 3;
+    int dest_base = 12;
+    const char* number = "120.11";
+
     char* result = student1_process(src_base, dest_base, number);
     if (result != NULL) {
-        printf("Результат преобразования: %s\n", result);
+        printf("%s\n", result);
         free(result);  
     } else {
-        puts("Ошибка преобразования!");
+        puts("Error occurred during conversion.");
     }
 
     return 0;
